479947354c89b51c328224917233b376063022f1
Anjaneshsurunida <s3468491@live.tees.ac.uk>
2025-03-29 05:43:51 +0530

    commit3

--- app/src/main/java/uk/ac/tees/mad/stox/model/repository/NetworkRepository.kt
+++
@@ -1,8 +0,0 @@
-package uk.ac.tees.mad.stox.model.repository
-
-import kotlinx.coroutines.flow.Flow
-import uk.ac.tees.mad.stox.model.network.NetworkConnectivityManager
-
-class NetworkRepository(private val networkConnectivityManager: NetworkConnectivityManager) {
-    val isNetworkAvailable: Flow<Boolean> = networkConnectivityManager.observeConnectivity()
-}
